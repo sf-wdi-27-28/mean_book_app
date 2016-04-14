7dea7e2c08fe7b2b6f4ec25aa6de61a822a6e75a
Cory Fauver <cofauver@gmail.com>
2016-04-13 17:26:38 -0700

    changing the router to ng-route rather than ui.route

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 angular
-  .module('bookApp', ['ui.router', 'ngResource'])
+  .module('bookApp', ['ngRoute', 'ngResource'])
   .config( BookConfig );
 
-  function BookConfig ( $locationProvider, $stateProvider, $urlRouterProvider ) {
-      // return to book-index if bad route request
-      $urlRouterProvider.otherwise("/");
-      $stateProvider
-        .state('home', {
-            url: '/',
-            template: "Home!"
-        })
-        .state('books-index', {
-            url: '/books',
-            templateUrl: "templates/books-index.html",
-            controller: "BooksController as bc"
-        });
+  function BookConfig ($routeProvider, $locationProvider){
+    $routeProvider
+      .when('/', {
+        controllerAs: 'booksCtrl',
+        controller: 'BooksController'
+      })
+      .when('/books', {
+        controllerAs: 'booksCtrl',
+        controller: 'BooksController',
+        templateUrl: 'templates/books-index.html'
+      });
       console.log("Config loaded.");
   };
+
+  // function BookConfig ( $locationProvider, $stateProvider, $urlRouterProvider ) {
+  //     // return to book-index if bad route request
+  //     $urlRouterProvider.otherwise("/");
+  //     $stateProvider
+  //       .state('home', {
+  //           url: '/',
+  //           template: "Home!"
+  //       })
+  //       .state('books-index', {
+  //           url: '/books',
+  //           templateUrl: "templates/books-index.html",
+  //           controller: "BooksController as bc"
+  //       });
+  //
+  // };
